0c05814068508a8c401efec9aa1acd6746319f52
AlexandreGGS <alexgeorges.sp@gmail.com>
2022-03-01 09:18:33 +0100

    Update functions.r

--- functions.r
+++ functions.r
@@ -651,7 +651,7 @@ font-family: Verdana;}
     pval <- obj$"p.value" ;
   } else if( method=="fisher") {
     cat("Analyse bivariée : Test de Fisher sur deux variables qualitatives<br>")
-    obj <- fisher.test(table(x,y), workspace = 2e12) ;
+    obj <- fisher.test(table(x,y), workspace = 200000000) ;
     print(obj) ;
     pval <- obj$"p.value" ;
   } else {
